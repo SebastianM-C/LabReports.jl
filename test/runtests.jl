5f2247525d964d4249d54bbf65aff0ef8ddc40de
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-04-02 00:21:37 +0300

    Add test data

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,13 @@ using LabReports
 using Test
 
 @testset "LabReports.jl" begin
-    # Write your own tests here.
+    const folder = "fake_data"
+
+    data = find_files(folder)
+
+    process_data("CV", data, (==, :Scan, 2))
+
+    process_data("EIS", data, (>, Symbol("-Phase (°)"), 0))
+
+    process_data("C&D", data, (0, 1.4, 0, 0, 0), Symbol("Time (s)"))
 end
