bf3a75a19f7dc1854c55fbe3dd4ca6c1ff5ade41
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-04-03 19:36:26 +0300

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,24 @@ using Test
 
 @testset "LabReports.jl" begin
     folder = "fake_data"
-    data = find_files(folder)
+    reference_folder = "reference_data"
+    to_rename = joinpath("fake_data", "200", "200_C&D_3.4e-3")
+    renamed = joinpath("fake_data", "200", "200_C&D_3.4e-3_D")
+
+    data = @test_logs (:info, "Renamed $to_rename to $renamed") find_files(folder)
+    reference_data = find_files(reference_folder)
+
+    @test length(data) == length(reference_data) == 3
+    @testset "Find $k" for k in keys(data)
+        for (datafile, ref) in zip(data[k], reference_data[k])
+            @test basename(datafile.filename) == basename(ref.filename)
+            @test basename(datafile.savename) == basename(ref.savename)
+            @test datafile.namemap == ref.namemap
+            @test datafile.units == ref.units
+            @test datafile.legend_units == ref.legend_units
+            @test datafile.idx == ref.idx
+        end
+    end
 
     process_data("CV", data, select=(:Scan, ==, 2))
 
@@ -11,6 +28,17 @@ using Test
 
     process_data("C&D", data, insert_D=(0, 1.4, 0, 0, 0), continue_col=Symbol("Time (s)"))
 
+    for ((root,dirs,files),(ref_root,ref_dirs,ref_files)) in zip(walkdir(folder), walkdir(reference_folder))
+        @test dirs == ref_dirs
+        @test files == ref_files
+        @testset "File comparison for $file" for (file, ref_file) in zip(files, ref_files)
+            f = read(joinpath(root, file), String)
+            r = read(joinpath(ref_root, ref_file), String)
+            @test f == r
+        end
+    end
+
+    # Cleanup
     clear(folder, r"filtered.*")
-    mv("fake_data\\200\\200_C&D_3.4e-3_D", "fake_data\\200\\200_C&D_3.4e-3")
+    mv(renamed, to_rename)
 end
