e520bf75a7718feb7bcf2635c840b876fa452456
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-04-17 01:01:43 +0300

    Add specific capacitance

--- src/analysis/capacitance.jl
+++ src/analysis/capacitance.jl
@@ -10,6 +10,17 @@ function capacitance(df, I)
     return ∫Vdt, 2*I*∫Vdt/ΔV^2
 end
 
+function specific_capacitance(C, porosity, folder)
+    A = 71u"cm^2"
+    a = (1.2*1.8)u"cm^2"
+
+    df = CSV.read("$folder/weights.csv")
+    mass = (df[!,:after].*u"g" .- df[!,:before].*u"g") .* a ./ A .|> u"μg"
+    mass_dict = Dict([10,30,50,70,90,110].=>mass[1:6])
+
+    C / mass_dict[porosity]
+end
+
 function dynamic_capacitance(df, I)
     t, V = df[!, Symbol("Time (s)")], df[!, Symbol("Potential (V)")]
     idxs = findall(v->v > 0, V)
@@ -23,17 +34,18 @@ function dynamic_capacitance(df, I)
     return itp.(ts), C
 end
 
-function add_capacitance!(Cs, datafile, df)
+function add_capacitance!(Cs, datafile, df, folder)
     I = parse(Float64, filevalue(datafile)) * u"A"
-    ∫Vdt, C = capacitance(df, I)
+    porosity = parse(Int, foldervalue(datafile))
+    ∫Vdt, C_abs = capacitance(df, I)
+    C = specific_capacitance(C_abs, porosity, folder)
 
-    push!(Cs[!, :Porosity], foldervalue(datafile))
     push!(Cs[!, :Area], ustrip(u"V*s", ∫Vdt))
     push!(Cs[!, :I], ustrip(u"mA", I))
-    push!(Cs[!, :C], ustrip(u"μF", C))
-end
+    push!(Cs[!, :C_abs], ustrip(u"μF", C_abs))
+    push!(Cs[!, :C], ustrip(u"F/g", C))
 
-function add_capacitance(datafile, df)
+    return nothing
 end
 
 function add_dyn_capacitance!(dyn_Cs, datafile, df)
@@ -48,14 +60,14 @@ end
 
 function compute_capacitances(data, folder)
     grouped = groupbyfolder(data["C&D"])
-    c_units = join(["-","V s","mA", replace_unicode("μF")], ',')
+    c_units = join(["V s","mA", replace_unicode("μF"), "F/g"], ',')
     dc_units = join(["-","V","mA", replace_unicode("μF")], ',')
 
     for (f, datafiles) in grouped
         capacitances = DataFrame(
-            :Porosity=>String[],
             :Area=>Float64[],
             :I=>Float64[],
+            :C_abs=>Float64[],
             :C=>Float64[])
         dynamic_capacitances = DataFrame(
             :Porosity=>String[],
@@ -66,7 +78,7 @@ function compute_capacitances(data, folder)
         for datafile in datafiles
             if endswith(datafile.filename, "_D")
                 df = read_file(datafile)
-                add_capacitance!(capacitances, datafile, df)
+                add_capacitance!(capacitances, datafile, df, folder)
                 add_dyn_capacitance!(dynamic_capacitances, datafile, df)
             end
         end
