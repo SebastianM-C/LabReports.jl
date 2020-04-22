9e9b9a2e92f7b8c8966d0fd31501eff929c77434
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-04-22 04:32:26 +0300

    Add power and energy

--- src/analysis/capacitance.jl
+++ src/analysis/capacitance.jl
@@ -6,8 +6,9 @@ function capacitance(df, I)
     ∫Vdt = integrate(t, V) * u"V*s"
     # ΔV = -(reverse(extrema(V))...)
     ΔV = 1.4u"V"
+    Δt = (t[end] - t[1]) * u"s"
 
-    return ∫Vdt, 2*I*∫Vdt/ΔV^2
+    return Δt, ∫Vdt, 2*I*∫Vdt/ΔV^2
 end
 
 function specific_capacitance(C, porosity, folder)
@@ -20,6 +21,13 @@ function specific_capacitance(C, porosity, folder)
 
     C / mass_dict[porosity]
 end
+
+function energy(C)
+    ΔV = 1.4u"V"
+    return (C*ΔV^2)/2
+end
+
+power(E, Δt) = E / Δt
 
 function dynamic_capacitance(df, I)
     t, V = df[!, Symbol("Time (s)")], df[!, Symbol("Potential (V)")]
@@ -37,13 +45,21 @@ end
 function add_capacitance!(Cs, datafile, df, folder)
     I = parse(Float64, filevalue(datafile)) * u"A"
     porosity = parse(Int, foldervalue(datafile))
-    ∫Vdt, C_abs = capacitance(df, I)
-    C = specific_capacitance(C_abs, porosity, folder)
+    Δt, ∫Vdt, C = capacitance(df, I)
+    C_specific = specific_capacitance(C, porosity, folder)
+    E = energy(C)
+    E_specific = energy(C_specific)
+    P = power(E, Δt)
+    P_specific = power(E_specific, Δt)
 
     push!(Cs[!, :Area], ustrip(u"V*s", ∫Vdt))
     push!(Cs[!, :I], ustrip(u"mA", I))
-    push!(Cs[!, :C_abs], ustrip(u"μF", C_abs))
-    push!(Cs[!, :C], ustrip(u"F/g", C))
+    push!(Cs[!, :C], ustrip(u"μF", C))
+    push!(Cs[!, :C_specific], ustrip(u"F/g", C_specific))
+    push!(Cs[!, :E], ustrip(u"W*hr", E))
+    push!(Cs[!, :E_specific], ustrip(u"W*hr/kg", E_specific))
+    push!(Cs[!, :P], ustrip(u"W", P))
+    push!(Cs[!, :P_specific], ustrip(u"W/kg", P_specific))
 
     return nothing
 end
@@ -60,15 +76,19 @@ end
 
 function compute_capacitances(data, folder)
     grouped = groupbyfolder(data["C&D"])
-    c_units = join(["V s","mA", replace_unicode("μF"), "F/g"], ',')
+    c_units = join(["V s","mA", replace_unicode("μF"), "F/g", "W*h", "Wh/kg", "W", "W/kg"], ',')
     dc_units = join(["-","V","mA", replace_unicode("μF")], ',')
 
     for (f, datafiles) in grouped
         capacitances = DataFrame(
             :Area=>Float64[],
             :I=>Float64[],
-            :C_abs=>Float64[],
-            :C=>Float64[])
+            :C=>Float64[],
+            :C_specific=>Float64[],
+            :E=>Float64[],
+            :E_specific=>Float64[],
+            :P=>Float64[],
+            :P_specific=>Float64[])
         dynamic_capacitances = DataFrame(
             :Porosity=>String[],
             :V=>Float64[],
@@ -83,12 +103,26 @@ function compute_capacitances(data, folder)
             end
         end
 
+        rename!(capacitances, Dict(
+            "E_specific"=>"Energy density",
+            "P_specific"=>"Power density",
+            "C"=>"C_abs",
+            "C_specific"=>"C"))
         sort!(capacitances, :I)
         sort!(dynamic_capacitances, :V)
-        comment = join(repeat([f*replace_powers(" mA cm^-2")], 4), ',')
 
-        write_file(capacitances, (c_units,comment), joinpath(folder, f, "capacitances.csv"), ',')
-        write_file(dynamic_capacitances, (dc_units,comment), joinpath(folder, f, "dynamic_capacitances.csv"), ',')
+        comment(units) = join(repeat([f*replace_powers(" mA cm^-2")], length(units)), ',')
+
+        write_file(
+            capacitances,
+            (c_units, comment(c_units)),
+            joinpath(folder, f, "capacitances.csv"),
+            ',')
+        write_file(
+            dynamic_capacitances,
+            (dc_units, comment(dc_units)),
+            joinpath(folder, f, "dynamic_capacitances.csv"),
+            ',')
     end
 
     return nothing
